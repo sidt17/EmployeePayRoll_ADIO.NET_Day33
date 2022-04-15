0f54f24ebc2e8610cebf048bfff491f74ec573d7
Siddhant <santoshshrivastava39@gmail.com>
2022-04-15 17:10:17 +0530

    Call in main method

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EmployeePayRollService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to program");
+            EmployeeRepo repo = new EmployeeRepo();
+            EmployeeModel employee = new EmployeeModel();
+
+            employee.EmployeeName = "Sameer Mark";
+            employee.PhoneNumber = "6575774758";
+            employee.Address = "ABC";
+            employee.Department = "HR";
+            employee.Gender = 'M';
+            employee.BasicPay = 2200.00;
+            employee.Deductions = 1500.00;
+            employee.TaxablePay = 200;
+            employee.Tax = 2500;
+            employee.NetPay = 250;
+           
+            employee.City = "Mumbai";
+            employee.Country = "India";
+            repo.AddEmployee(employee);
+          
+        }
+    }
+}
